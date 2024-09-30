d298db792a2c0263755b72ac0fbf1e0b47e44cb7
박철현 <park82602@gmail.com>
2024-09-30 13:28:29 +0900

    first commit

---
+++ src/main/java/com/cheolhyeon/weatherapi_mission/WeatherApiMissionApplication.java
@@ -0,0 +1,13 @@
+package com.cheolhyeon.weatherapi_mission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherApiMissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherApiMissionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cheolhyeon/weatherapi_mission/WeatherApiMissionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cheolhyeon.weatherapi_mission;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherApiMissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
